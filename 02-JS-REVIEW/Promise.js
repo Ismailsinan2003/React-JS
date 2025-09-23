717ffbdb65557f6e1df73126d93d946b85470737
Ismail Sinan <ismailsinan2003@gmail.com>
2025-09-23 11:52:08 +0400

    JS Review

---
+++ 02-JS-REVIEW/Promise.js
@@ -0,0 +1,13 @@
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// console.log("sinan");
+
+/* async function fetchData(URL) {
+  const res = await fetch(URL);
+  const data = await res.json();
+  console.log(data);
+}
+
+fetchData("https://jsonplaceholder.typicode.com/todos/1").then( ()=> console.log("sinan")) */
