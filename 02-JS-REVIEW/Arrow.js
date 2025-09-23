1181e5034646da58971ebd20ee65fd3150ced5e7
Ismail Sinan <ismailsinan2003@gmail.com>
2025-09-23 07:40:51 +0400

    JS Review

---
+++ 02-JS-REVIEW/Arrow.js
@@ -0,0 +1,183 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+// 1. Get an array of all authors.
+
+/* const autors = data.map((book) => book.author);
+console.log(autors); */
+
+//2. Get an array of all book IDs.
+
+/* const ids = data.map( (book) => book.id)
+console.log(ids) */
+
+//3.Find all books with more than 500 pages.
+
+/* const largeBooks = data.filter((book) => book.pages > 500);
+console.log(largeBooks); */
+
+//4. Return a new array of all titles in uppercase
+
+/* const titles = data.map((book) => book.title.toUpperCase());
+console.log(titles) */
+
+//5.Filter books that have a movie adaptation.
+
+/* const hasAdoptation = data.filter((book) => book.hasMovieAdaptation == true);
+console.log(hasAdoptation); */
+
+//6. Find the book with id = 4.
+
+/* const book = data.find( (book) => book.id === 4)
+console.log(book) */
+
+// 7. Count the total pages of all books combined.
+
+/* const TotalPages = data.reduce((acc, val) => {
+  acc += val.pages;
+  return acc;
+}, 0);
+
+console.log(TotalPages); */
+
+// 8. Sort the books by publication year (ascending).
+
+/* const sortedByYear = data.slice().sort((a, b) => {
+  return new Date(a.publicationDate) - new Date(b.publicationDate);
+});
+
+console.log(sortedByYear.map(book => book.title)); */
