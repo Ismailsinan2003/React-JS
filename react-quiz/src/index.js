186c69a5c3155b29011b21c11206a4bd3cf807d4
Ismail Sinan <ismailsinan2003@gmail.com>
2025-11-29 18:37:16 +0400

    React Quiz project

---
+++ react-quiz/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import DateCounter from "./DateCounter";
+// import Error from "./Error";
+// import Header from "./Header.js";
+// import Loader from "./Loader.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
