19edab0373719f6469546e2e896870b7045cd229
Ismail Sinan <ismailsinan2003@gmail.com>
2025-11-30 15:01:05 +0400

    added useReducer hook

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 // import DateCounter from "./DateCounter";
 // import Error from "./Error";
 // import Header from "./Header.js";
