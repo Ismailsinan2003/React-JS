3635a1e68d45435b03912af133d33ab8e89515a1
Ismail Sinan <ismailsinan2003@gmail.com>
2025-12-09 14:40:03 +0400

    setting up Query String

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,7 +1,24 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  console.log(lat);
+  console.log(lng);
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Maps</h1>
+      <h2>
+        position :{lat} {lng}
+      </h2>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 40 })}>
+        change POS
+      </button>
+    </div>
+  );
 }
 
 export default Map;
