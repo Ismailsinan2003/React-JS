2f20cfe7952c37418d2c14f4dc01ee1ecf29c9f6
Ismail Sinan <ismailsinan2003@gmail.com>
2025-12-13 15:52:07 +0400

    added programmativ navigation

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -14,7 +14,7 @@ function CityItem({ city }) {
     <li>
       <Link
         className={styles.cityItem}
-        to={`${id}?lat=${position.lat}&lng=${position.long}`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
