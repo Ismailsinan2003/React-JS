9c4b3f757392717881e6c12fd65d9c79a9bc3ab2
Ismail Sinan <ismailsinan2003@gmail.com>
2025-12-14 13:58:29 +0400

    built atomic blog application

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -8,8 +8,7 @@ function Map() {
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  console.log(lat);
-  console.log(lng);
+
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Maps</h1>
